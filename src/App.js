a73241517f85b69e8d3672cc5b2bc1a75350a915
woorix2 <woori8806@naver.com>
2022-04-10 18:26:40 +0900

    2nd cmt update css module

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
 import { useEffect, useState } from "react";
 import Money from "./Money";
+import style from './App.module.css';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBitcoinSign } from "@fortawesome/free-solid-svg-icons";
 
 function App(){
   const [loading, setLoading] = useState(true);
@@ -21,23 +24,28 @@ function App(){
   const reset= () => setAmount(0);
 
   return (
-      <div>
-        <h1> 코인? 얼마면 돼? </h1>
-        <hr />
-        <div>
-            <label htmlFor="won">가진 금액(원):</label>
-            <input value={amount} id="won" placeholder="WON" type="number" onChange={onChange}  />
+    <div className={style.wrapper}>
+      <div className={style.content}>
+        <div className={style.header}>
+        <FontAwesomeIcon icon={ faBitcoinSign } className={style.icon}/>
+        <h1 className={style.title}> 코인? 얼마면 돼? </h1>
+        <p className={style.des}>가진 돈을 달러로 환산해 보고 원하는 코인을 몇 개나 살 수 있는지 알아보세요!</p>
         </div>
-        <div>
-            <label htmlFor="dollor">USD:</label>
-            <input defaultValue={ loading ? "" : amount / moneys.quotes.USDKRW} id="dollor" placeholder="USD" type="text" />
-            <button onClick={reset}>Reset</button>
+        <div className={style.firstContent}>
+          <div className={style.inputBox}>
+              <label htmlFor="won">금액(원):</label>
+              <input className={style.input} value={amount} id="won" placeholder="WON" type="number" onChange={onChange}  />
+          </div>
+          <div className={style.inputBox}>
+              <label htmlFor="dollor">USD:</label>
+              <input className={style.input} defaultValue={ loading ? "" : amount / moneys.quotes.USDKRW} id="dollor" placeholder="USD" type="text"/>
+              <button className={style.btn} onClick={reset}>Reset</button>
+          </div>
         </div>
         
         <Money won={amount} moneys={moneys} />
       </div>
-
-      
+    </div>
   )
 }
 export default App;
--- src/Money.js
+++ src/Money.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from "react";
+import style from './App.module.css';
 
 function Money(props) {
     const [loading, setLoading] = useState(true);
@@ -17,8 +18,6 @@ function Money(props) {
     const onChange = (e) => {
         setCoinUsd(e.target.value);
         console.log(e.target.value)
-        // setNumCoin((props.won / props.moneys.quotes.USDKRW) / coinUsd)
-        // console.log(numCoin)
     }
     const onChangeNum = () => {
         setNumCoin(((props.won) / (props.moneys.quotes.USDKRW)) / (coinUsd));
@@ -28,13 +27,12 @@ function Money(props) {
     console.log(coinUsd)
     return (
         <div>
-            <br />
-            <div>
-                <h2>{ loading ? "" : `전체 ${coins.length}개의 코인`}</h2>
-                <div>
+            <div className={style.secondContent} >
+                <h3 className={style.subTitle}>{ loading ? "" : `전체 ${coins.length}개의 코인`}</h3>
+                <div className={style.inputBox}>
                 {loading ? <strong>Loading...</strong> : 
-                <select  onChange={onChange}>
-                    <option>코인을 골라보세요</option>
+                <select className={style.select}  onChange={onChange}>
+                    <option>코인을 선택하세요</option>
                     {coins.map((coin) => 
                     <option key={coin.id} value={coin.quotes.USD.price}>
                         {coin.name} ({coin.symbol}): {coin.quotes.USD.price}
@@ -42,11 +40,13 @@ function Money(props) {
                     </option>) }
                 </select>}
                 </div>
-                <label htmlFor="numCoin">살 수 있는 갯수:</label>
-                <input id="numCoin" type="text" placeholder="과연 몇개 살 수 있을까?" 
-                value={numCoin} 
-                />
-                <button onClick={onChangeNum}>변환</button>
+                <div className={style.inputBox}>
+                    <label htmlFor="numCoin">살 수 있는 갯수:</label>
+                    <input className={style.input} id="numCoin" type="text" placeholder="과연 몇개 살 수 있을까?" 
+                    value={numCoin} disabled/>개
+                    <button className={style.btn} onClick={onChangeNum}>조회</button>
+                </div>
+                
             </div>
         </div>
     ) 

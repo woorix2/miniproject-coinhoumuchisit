740ed43e006e4757c3d3d13be6c0595d0a39eca0
woorix2 <woori8806@naver.com>
2022-05-19 23:29:24 +0900

    29th cmt Refactor: ESLint, Prettier ESLint, Prettier 설치 후 적용 코드 포맷팅

--- src/App.js
+++ src/App.js
@@ -1,82 +1,95 @@
-import { useEffect, useState, useRef } from "react";
+import React, { useEffect, useState, useRef } from "react";
 import Money from "./Money";
-import style from './App.module.css';
-import Coin from './coin.png'
+import style from "./App.module.css";
+import Coin from "./coin.png";
 
-function App(){
+function App() {
   const [loading, setLoading] = useState(true);
   const [moneys, setMoneys] = useState([]);
-  const [amount, setAmount] = useState('');
-  const [usd, setUsd] = useState('');
+  const [amount, setAmount] = useState("");
+  const [usd, setUsd] = useState("");
   const inputRef = useRef();
 
-  useEffect( () => {
-      fetch(`http://api.currencylayer.com/live?access_key=cacc01bd9144e3546bed9bf5e74cb390&format=1`)
+  useEffect(() => {
+    fetch(
+      `http://api.currencylayer.com/live?access_key=cacc01bd9144e3546bed9bf5e74cb390&format=1`
+    )
       .then((response) => response.json())
-      .then((json) => { 
-          setMoneys(json)
-          setLoading(false)
-          inputRef.current.focus();
+      .then((json) => {
+        setMoneys(json);
+        setLoading(false);
+        inputRef.current.focus();
       })
-      .catch( err => {
+      .catch((err) => {
         console.log(err);
-      })
-    } ,[])
-  
+      });
+  }, []);
+
   const getWon = (e) => {
     const value = e.target.value;
     const removedCommaValue = Number(value.replaceAll(",", ""));
     setTimeout(setAmount(removedCommaValue.toLocaleString()), 1000);
-  }
-  const reset= () => {
-    setAmount('');
-    setUsd('');
+  };
+  const reset = () => {
+    setAmount("");
+    setUsd("");
     inputRef.current.focus();
-  }
-  
-  const removeComma = parseInt(amount.replace(/,/g , ''));
+  };
+
+  const removeComma = parseInt(amount.replace(/,/g, ""));
 
   const getUSD = () => {
-    setUsd( loading ? "" : (amount === '') ? "" : removeComma / moneys.quotes.USDKRW);
-  }
+    setUsd(
+      loading ? "" : amount === "" ? "" : removeComma / moneys.quotes.USDKRW
+    );
+  };
 
   return (
     <div className={style.wrapper}>
       <div className={style.content}>
         <div className={style.header}>
-        <img src={Coin} alt="로고이미지" className={style.icon}/>
-        <h1 className={style.title}> 코인? 얼마면 돼? </h1>
-        <p className={style.des}>가진 돈을 달러로 환산해 원하는 코인을 몇 개나 살 수 있는지 알아보세요!</p>
+          <img src={Coin} alt="로고이미지" className={style.icon} />
+          <h1 className={style.title}> 코인? 얼마면 돼? </h1>
+          <p className={style.des}>
+            가진 돈을 달러로 환산해 원하는 코인을 몇 개나 살 수 있는지
+            알아보세요!
+          </p>
         </div>
         <div className={style.firstContent}>
           <div className={style.inputBox1}>
-              <label htmlFor="won">금액(원):</label>
-              <input 
-              ref={inputRef} 
-              className={`${style.input} ${style.priceInput}`} 
-              value={amount} 
-              id="won" 
-              placeholder="WON" 
-              type="text" 
-              onChange={getWon} />
+            <label htmlFor="won">금액(원):</label>
+            <input
+              ref={inputRef}
+              className={`${style.input} ${style.priceInput}`}
+              value={amount}
+              id="won"
+              placeholder="WON"
+              type="text"
+              onChange={getWon}
+            />
           </div>
           <div className={style.inputBox1}>
-              <label htmlFor="dollor">USD:</label>
-              <input 
+            <label htmlFor="dollor">USD:</label>
+            <input
               className={`${style.input} ${style.usdInput}`}
               defaultValue={usd}
-              id="dollor" 
-              placeholder="USD" 
+              id="dollor"
+              placeholder="USD"
               type="text"
-              readOnly/>
+              readOnly
+            />
           </div>
-          <button className={style.btn} onClick={getUSD} >Check</button>
-          <button className={style.btn} onClick={reset}>Reset</button>
+          <button className={style.btn} onClick={getUSD}>
+            Check
+          </button>
+          <button className={style.btn} onClick={reset}>
+            Reset
+          </button>
         </div>
-        
+
         <Money amount={amount} won={removeComma} moneys={moneys} />
       </div>
     </div>
-  )
+  );
 }
 export default App;
--- src/Money.js
+++ src/Money.js
@@ -1,57 +1,79 @@
 import React, { useState, useEffect } from "react";
-import style from './App.module.css';
+import style from "./App.module.css";
 
-const Money = (props) => {
-    const [loading, setLoading] = useState(true);
-    const [coins, setCoins] = useState([]) // 빈 배열을 초깃값으로 해서 undefined가 뜨지 않게 함.
-    const [coinUsd, setCoinUsd] = useState(0);
-    const [numCoin, setNumCoin] = useState('');
+// eslint-disable-next-line react/prop-types
+const Money = ({ amount, won, moneys }) => {
+  const [loading, setLoading] = useState(true);
+  const [coins, setCoins] = useState([]); // 빈 배열을 초깃값으로 해서 undefined가 뜨지 않게 함.
+  const [coinUsd, setCoinUsd] = useState(0);
+  const [numCoin, setNumCoin] = useState("");
 
-    useEffect( () => {
-        fetch('https://api.coinpaprika.com/v1/tickers?limit=500')
-        .then((response) => response.json())
-        .then((json) => { 
-        setCoins(json)
-        setLoading(false)
-        console.log("fetch")
-        })
-    } ,[])
+  useEffect(() => {
+    fetch("https://api.coinpaprika.com/v1/tickers?limit=500")
+      .then((response) => response.json())
+      .then((json) => {
+        setCoins(json);
+        setLoading(false);
+        console.log("fetch");
+      });
+  }, []);
 
-    const getCoin = (e) => {
-        setCoinUsd(e.target.value);
+  const getCoin = (e) => {
+    setCoinUsd(e.target.value);
+  };
+  const onChangeNum = () => {
+    // eslint-disable-next-line react/prop-types
+    if (amount.length < 1) {
+      alert("금액을 입력하세요");
     }
-    const onChangeNum = () => {
-        if(props.amount.length < 1) {
-            alert("금액을 입력하세요");
-        }
-        setNumCoin(((props.won) / (props.moneys.quotes.USDKRW)) / (coinUsd));
-    }
-    const reset= () => setNumCoin('');
+    // eslint-disable-next-line react/prop-types
+    setNumCoin(won / moneys.quotes.USDKRW / coinUsd);
+  };
+  const reset = () => setNumCoin("");
 
-    return (
-        <div>
-            <div className={style.secondContent} >
-                <h3 className={style.subTitle}>{ loading ? "" : `전체 ${coins.length}개의 코인`}</h3>
-                <div className={style.inputBox}>
-                {loading ? <strong>Loading...</strong> : 
-                <select className={style.select}  onChange={getCoin}>
-                    <option defaultValue={numCoin === '' ? true : false}>코인을 선택하세요</option>
-                    {coins.map((coin) => 
-                    <option key={coin.id} value={coin.quotes.USD.price}>
-                        {coin.name} ({coin.symbol}): {coin.quotes.USD.price}
-                        USD
-                    </option>) }
-                </select>}
-                </div>
-                <div className={style.inputBox}>
-                    <label htmlFor="numCoin">개수:</label>
-                    <input className={`${style.input} ${style.coinInput}`} id="numCoin" type="text" placeholder="과연 몇개 살 수 있을까?" 
-                    value={numCoin} disabled/>개
-                </div>
-                <button className={style.btn} onClick={onChangeNum}>Check</button>
-                <button className={style.btn} onClick={reset}>Reset</button>
-            </div>
+  return (
+    <div>
+      <div className={style.secondContent}>
+        <h3 className={style.subTitle}>
+          {loading ? "" : `전체 ${coins.length}개의 코인`}
+        </h3>
+        <div className={style.inputBox}>
+          {loading ? (
+            <strong>Loading...</strong>
+          ) : (
+            <select className={style.select} onChange={getCoin}>
+              <option defaultValue={numCoin === "" ? true : false}>
+                코인을 선택하세요
+              </option>
+              {coins.map((coin) => (
+                <option key={coin.id} value={coin.quotes.USD.price}>
+                  {coin.name} ({coin.symbol}): {coin.quotes.USD.price}
+                  USD
+                </option>
+              ))}
+            </select>
+          )}
         </div>
-    ) 
+        <div className={style.inputBox}>
+          <label htmlFor="numCoin">개수:</label>
+          <input
+            className={`${style.input} ${style.coinInput}`}
+            id="numCoin"
+            type="text"
+            placeholder="과연 몇개 살 수 있을까?"
+            value={numCoin}
+            disabled
+          />
+          개
+        </div>
+        <button className={style.btn} onClick={onChangeNum}>
+          Check
+        </button>
+        <button className={style.btn} onClick={reset}>
+          Reset
+        </button>
+      </div>
+    </div>
+  );
 };
 export default React.memo(Money);
